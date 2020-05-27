cb892fdf3a227ef227b89053df5f41da310c2f48
Michelle Hu <jmhu@uw.edu>
2020-05-27 11:22:59 -0700

    bin/utm_convert.py: modularize code, add utm zone return

--- bin/utm_convert.py
+++ bin/utm_convert.py
@@ -1,38 +1,19 @@
 #!/usr/bin/env python
-# This script returns utm zone numbers for the WGS84 datum as well as EPSG codes for the contiguous United states.  The utm package is required
-### # TODO:
-# Incorporate handling for one-off lat lon inputs
-# bounds rounded to the  that intersect the spatial extent extracted from geotransform information of an input image.  Large parts of this code are sourced from David Shean's dgtools repository and from https://gis.stackexchange.com/questions/57834/how-to-get-raster-corner-coordinates-using-python-gdal-bindings
+
+# This script returns EPSG codes and utm zone numbers for the WGS84 datum.
+# Parts of this code are sourced from:
+# David Shean's dgtools repository, 
+# https://gis.stackexchange.com/questions/57834/how-to-get-raster-corner-coordinates-using-python-gdal-bindings, 
+# and https://github.com/DigitalGlobe/gdal_ortho/blob/master/gdal_ortho/gdal_ortho.py
 
 '''Usage from command line:
-python utm_convert.py -in image.tif -L left -B bottom -R right -T top
-
-output format
-text file of corresponding utm zones 
+utm_convert.py -in image.tif -L left -B bottom -R right -T top -z T/F (get_utm_zone) -c T/F (get_projected_coordinates)
+output format: print epsg and utm zones 
 '''
 
 import argparse, gdal, osr, math
-import sys
+import sys, os
 
-parser = argparse.ArgumentParser(description='Geographic coordinates to UTM zone converter')
-parser.add_argument('-in', '--input_file', help='GeoTiff image file', required=False)
-parser.add_argument('-L', '--left', help='Leftmost bound', required=False, type=float)
-parser.add_argument('-B', '--bottom', help='Bottom-most bound', required=False, type=float)
-parser.add_argument('-R', '--right', help='Rightmost bound', required=False, type=float)
-parser.add_argument('-T', '--top', help='Top bound', required=False, type=float)
-parser.add_argument('-zone', '--get_utm_zone', help='Flag to block returning UTM zone', required=False)
-parser.add_argument('-coords', '--convert_coords', help='Flag to convert coordinates to UTM', required=False)
-
-args = parser.parse_args()
-in_fn = args.input_file
-l=args.left
-b=args.bottom
-r=args.right
-t=args.top
-z=args.get_utm_zone
-c=args.convert_coords
-
-# Define functions
 def round_down(n, decimals=2):
     '''Function to implement floor function and round down to nearest hundredths place'''
     multiplier = 10 ** decimals
@@ -63,22 +44,16 @@ def getAllTag(xml_fn, tag):
     '''From David Shean's dgtools'''
     import xml.etree.ElementTree as ET
     tree = ET.parse(xml_fn)
-
-    # Want to check to make sure tree contains tag
     elem = tree.findall('.//%s' % tag)
     return [i.text for i in elem]
 
-
 def GetExtent(gt, cols, rows):
-    '''
-    Get spatial extent of input raster based on geotransform information.
-    '''
-    # corners:   coordinates of each corner (CCW): TL, BL, BR, TR
+    '''Get spatial extent of input raster based on geotransform information.
+    corners:   coordinates of each corner (CCW): TL, BL, BR, TR'''
     gdal_ext=[]
     corners=[]
     xarr=[0,cols]
     yarr=[0,rows]
-
     for px in xarr:
         for py in yarr:
             x=gt[0]+(px*gt[1])+(py*gt[2])
@@ -86,12 +61,10 @@ def GetExtent(gt, cols, rows):
             corners.append([x,y])
         yarr.reverse()
     gdal_ext=[corners[0][0], corners[2][1], corners[2][0], corners[0][1]] # L, B, R, T
-
     return gdal_ext, corners
 
 def ReprojectCoords(coords, src_srs, tgt_srs):
     ''' Function to reproject a list of x,y coordinates.
-
         @type geom:     C{tuple/list}
         @param geom:    List of [[x,y],...[x,y]] coordinates
         @type src_srs:  C{osr.SpatialReference}
@@ -108,11 +81,10 @@ def ReprojectCoords(coords, src_srs, tgt_srs):
         trans_coords.append([x,y])
     return trans_coords
 
-def get_utm_epsg_code(lat, lon):
+def get_utm_epsg_code(lat, lon, z=None):
     import shapely.geometry
     import json
     """Modified from https://github.com/DigitalGlobe/gdal_ortho/blob/master/gdal_ortho/gdal_ortho.py
-    
     Looks up the UTM zone for a point. This function uses the UTM Zone Boundaries shapefile from this
     location: http://earth-info.nga.mil/GandG/coordsys/grids/universal_grid_system.html
     Args:
@@ -120,7 +92,8 @@ def get_utm_epsg_code(lat, lon):
         lon: Longitude of the point to use.
     Returns the integer EPSG code for the UTM zone containing the input point.
     """
-    UTM_ZONES_PATH="/mnt/Backups/jmhu/git_dirs/rs_tools/bin/UTM_Zone_Boundaries.geojson"
+
+    UTM_ZONES_PATH="/mnt/Backups/jmhu/git_dirs/rs_tools/data/UTM_Zone_Boundaries.geojson"
     
     # Load the UTM zones
     zone_geoms = {}
@@ -140,31 +113,24 @@ def get_utm_epsg_code(lat, lon):
     if found_zone is None:
         raise InputError("Latitude %.10f Longitude %.10f is not in any UTM zone" % \
                          (lat, lon))
-
     # Parse the zone
     (zone_num, hemisphere) = found_zone.split(",")
     if hemisphere == "n":
         base_epsg = 32600
     else:
         base_epsg = 32700
+    if z is not None:
+        print("ZONE:", zone_num + hemisphere.upper())
+        print("EPSG:", base_epsg + int(zone_num))    
+    else:
+        print(base_epsg + int(zone_num))    
 
-    epsg = base_epsg + int(zone_num)
-    print(epsg)    
-    return epsg
-
-
-if z is None:
+def run(in_fn=None, l=None, b=None, r=None, t=None, z=None, c=None):
     try:
-        try:
+        if os.path.exists(in_fn[:-3]+'xml'):
             xml = in_fn[:-3]+'xml'
-        # if in_fn[-3:] == 'XML':
-        #     xml=in_fn
-        # else:
-        #     xml=in_fn[:-3]+'XML'
-            f=open(xml)
-            f.close()
-        except:
-            xml = in_fn[:-3]+'XML'
+        elif os.path.exists(in_fn[:-3]+'XML'):
+            xml = in_fn[:-3]+'XML'    
         ur_lon=float(getTag(xml, 'URLON'))
         ur_lat=float(getTag(xml, 'URLAT'))
 
@@ -182,7 +148,6 @@ if z is None:
         ymin=int(round_down(min(lr_lat, ll_lat), decimals=0))  # Bottom
         xmax=int(round_up(max(ur_lon, lr_lon), decimals=0))    # Right
         ymax=int(round_up(max(ul_lat, ur_lat), decimals=0))    # Top
-        
     except:
         if l is not None:
             xmin, ymin, xmax, ymax=l, b, r, t
@@ -198,7 +163,6 @@ if z is None:
 
             src_srs=osr.SpatialReference()
             src_srs.ImportFromWkt(raster_ds.GetProjection())
-            print(src_srs.ImportFromWkt(raster_ds.GetProjection()))
             tgt_srs=src_srs.CloneGeogCS()
 
             geo_ext=ReprojectCoords(corners,src_srs,tgt_srs)
@@ -216,21 +180,44 @@ if z is None:
         xmin=xmin-360
     if xmax>180:
         xmax=xmax-360
-
+        
     # Get center coordinates and pull UTM zone from these
     x_center=(xmin + xmax)/2
     y_center=(ymin + ymax)/2
-
-#     print(xmin, ymin, xmax, ymax)
-#     print(x_center, y_center)
-    get_utm_epsg_code(y_center, x_center)
     
-# if c is not None:
-#     import utm
-#     xmin=l
-#     ymin=b
-#     xmax=r
-#     ymax=t
-#     min_easting, min_northing, _, _=utm.from_latlon(ymin, xmin)
-#     max_easting, max_northing, _, _=utm.from_latlon(ymax, xmax)
-#     print(min_easting, min_northing, max_easting, max_northing)
+    get_utm_epsg_code(y_center, x_center, z)
+    
+    # If utm package has been installed, convert input coordinates to projected UTM coordinates
+    if c is not None:
+        import utm
+        xmin, ymin, xmax, ymax=l, b, r, t
+        min_easting, min_northing, _, _=utm.from_latlon(ymin, xmin)
+        max_easting, max_northing, _, _=utm.from_latlon(ymax, xmax)
+        print(min_easting, min_northing, max_easting, max_northing)
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Geographic coordinates to UTM zone converter')
+    parser.add_argument('-in', '--input_file', help='GeoTiff image file', required=False)
+    parser.add_argument('-L', '--left', help='Leftmost bound', required=False, type=float)
+    parser.add_argument('-B', '--bottom', help='Bottom-most bound', required=False, type=float)
+    parser.add_argument('-R', '--right', help='Rightmost bound', required=False, type=float)
+    parser.add_argument('-T', '--top', help='Top bound', required=False, type=float)
+    parser.add_argument('-zone', '--get_utm_zone', help='Flag to block returning UTM zone', required=False)
+    parser.add_argument('-coords', '--convert_coords', help='Flag to convert coordinates to UTM', required=False)
+    return parser
+        
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    in_fn = args.input_file
+    l=args.left
+    b=args.bottom
+    r=args.right
+    t=args.top
+    z=args.get_utm_zone
+    c=args.convert_coords
+    
+    run(in_fn=in_fn, l=l, b=b, r=r, t=t, z=z, c=c)
+    
+if __name__ == "__main__":    
+    main()
